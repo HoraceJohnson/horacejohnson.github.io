7193d8d581df2429eae299445adb294f369e0d87
HoraceJohnson <65810498+HoraceJohnson@users.noreply.github.com>
2020-11-24 13:48:56 +0630

    Update time.js

--- time.js
+++ time.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Sep 23, 2020 15:37:25").getTime();
+var countDownDate = new Date("Sep 26, 2021 15:37:25").getTime();
 
 var countdownfunction = setInterval(function() {
 
